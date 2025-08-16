bb2200a67bfa9dc841ad3ade49ce0fe21846ba71
fopo2025 <fopo2025@outlook.com>
2025-08-16 18:13:10 +0000

    cat /

--- src/COBOL/Hello-World.cob
+++ src/COBOL/Hello-World.cob
@@ -1,7 +1,9 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID HELLOWORLD.
+    *>       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Hello-World.
 
        PROCEDURE DIVISION.
            DISPLAY "Hello, COBOL world!"
 
            STOP RUN.
+
+       END PROGRAM Hello-World.
